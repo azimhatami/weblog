5b0a33f21f419916cad2dcd1c72398b852d70c3a
Azim Hatami <azimhatami.dev@gmail.com>
2025-06-01 07:09:19 +0330

    Created dashboard UI

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,6 +2,17 @@ const { Router } = require('express');
 
 const router = new Router();
 
+
+// @desc Dashboard
+// @route GET /dashboard
+router.get('/', (req, res) => {
+  res.render('dashboard', {
+    pageTitle: 'بخش مدیریت | داشبورد',
+    path: '/dashboard',
+    layout: './layouts/dashLayout',
+  });
+});
+
 // @desc Login Page
 // @route GET /dashboard/login
 router.get('/login', (req, res) => {
