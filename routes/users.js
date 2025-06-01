1b6da8adacb2a8a0574b1f9c965c3dd49526c3ad
Azim Hatami <azimhatami.dev@gmail.com>
2025-06-01 22:22:28 +0330

    Refactor: Switch from Yup to Fastest-Validator for data validation

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,55 @@
 const { Router } = require('express');
-const Yup = require('yup');
+const Validator = require('fastest-validator');
 
 
 const router = new Router();
+const v = new Validator();
 
-const schema = Yup.object().shape({
-  fullname: Yup.string().required('نام و نام خانوادگی الزامی می باشد').min(4, "نام و نام خانوادگی نباید کمتر از ۴کاراکتر باشد").max(255, "نام و نام خانوادگی نباید بیشتر از ۲۵۵ کاراکتر باشد"),
-  email: Yup.string().email('ایمیل معتبر نمی باشد').required('ایمیل الزامی می باشد'),
-  password: Yup.string().min(4, "کلمه عبور نباید کمتر از ۴ کارکتر باشد").max(255, "کلمه عبور نباید بیشتر از ۲۵۵ کاراکتر باشد").required("کلمه عبور الزامی می باشد"),
-  confirmPassword: Yup.string().required("تکرار کلمه عبور الزامی می باشد").oneOf([Yup.ref('password'), null])
-});
+const schema = {
+  fullname: {
+    type: 'string',
+    trim: true,
+    max: 255,
+    min: 4,
+    messages: {
+      required: 'نام و نام خانوادگی الزامی می باشد',
+      stringMin: 'نام و نام خانوادگی نباید کمتر از ۴ کاراکتر باشد',
+      stringMax: 'نام و نام خانوادگی نباید بیشتر از ۲۵۵ کاراکتر باشد'
+    }
+  },
+  email: {
+    type: 'email',
+    normalize: true,
+    messages: {
+      emailEmpty: 'فیلد ایمیل نباید خالی باشد',
+      required: 'ایمیل الزامی می باشد',
+      string: 'آدرس ایمیل را بررسی کنید'
+    }
+  },
+  password: {
+    type: 'string',
+    min: 4,
+    max: 255,
+    messages: {
+      required: 'کلمه عبور الزامی می باشد',
+      string: 'کلمه عبور را بررسی کنید',
+      stringMin: 'کلمه عبور نباید کمتر از ۴ کاراکتر باشد',
+      stringMax: 'کلمه عبور نمی تواند بیشتر از ۲۵۵ کاراکتر باشد'
+    }
+  },
+  confirmPassword: {
+    type: 'string',
+    min: 4,
+    max: 255,
+    messages: {
+      required: 'تکرار کلمه عبور الزامی می باشد',
+      string: 'تکرار کلمه عبور را بررسی کنید',
+      stringMin: 'تکرار کلمه عبور نباید کمتر از ۴ کاراکتر باشد',
+      stringMax: 'تکرار کلمه عبور نمی تواند بیشتر از ۲۵۵ کاراکتر باشد'
+    }
+  }
+};
+
 
 // @desc Login Page
 // @route GET /users/login
@@ -32,17 +72,27 @@ router.get('/register', (req, res) => {
 // @desc Register Handle
 // @route POST /users/register
 router.post('/register', (req, res) => {
-  schema.validate(req.body).then((result) => {
+  const validate = v.validate(req.body, schema);
+  const errorArr = [];
+  if (validate === true) {
+    const {fullname, email, password, confirmPassword} = req.body;
+    if (password !== confirmPassword) {
+      errorArr.push({message: 'کلمه های عبور یکسان نیستند'});
+
+      return res.render('register', {
+        pageTitle: 'ثبت نام کاربر',
+        path: '/register',
+        errors: errorArr
+      });
+    }
     res.redirect('/users/login');
-  }).catch((err) => {
-    console.log(err);
-    console.log(err.errors);
+  } else {
     res.render('register', {
-      pageTitle: 'ثبت نام کاربر', 
-      path: '/register', 
-      errors: err.errors
+      pageTitle: 'ثبت نام کاربر',
+      path: '/register',
+      errors: validate,
     });
-  })
+  }
 });
 
 
