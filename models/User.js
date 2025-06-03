aa0d1028047830154634f13e6e3bb3621c240b5c
Azim Hatami <azimhatami.dev@gmail.com>
2025-06-03 09:03:41 +0330

    Implement user controller

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require('mongoose');
-const Yup = require('yup');
+
+const { schema } = require('./secure/userValidation');
 
 const userSchema = new Schema({
   fullname: {
@@ -24,22 +25,6 @@ const userSchema = new Schema({
   }
 });
 
-const schema = Yup.object().shape({
-  fullname: Yup.string()
-    .required('نام و نام خانوادگی الزامی می باشد')
-    .min(4, 'نام و نام خانوادگی نباید کمتر از ۴ کاراکتر باشد')
-    .max(255, 'نام و نام خانوادگی نباید بیشتر از۲۵۵ کاراکتر باشد'),
-  email: Yup.string()
-    .email('ایمیل معتبر نمی باشد')
-    .required('ایمیل الزامی می باشد'),
-  password: Yup.string()
-    .min(4, 'کلمه عبور نباید کمتر از ۴ کاراکتر باشد')
-    .max(255, 'کلمه عبور نباید بیشتر از ۲۵۵ کاراکتر باشد')
-    .required('کلمه عبور الزامی است'),
-  confirmPassword: Yup.string()
-    .required('تکرار کلمه عبور الزامی می باشد')
-    .oneOf([Yup.ref('password'), null]),
-});
 
 userSchema.statics.userValidation = function(body) {
   return schema.validate(body, {abortEarly: false})
