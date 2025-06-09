ab7d1d8511a92aac0dc36eca3e49eafe4a79c155
Azim Hatami <azimhatami.dev@gmail.com>
2025-06-09 12:18:21 +0330

    Implement image compression using Sharp

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,6 @@
 const multer = require('multer');
+const sharp = require('sharp');
+const uuid = require('uuid').v4;
 
 const Blog = require('../models/Blog');
 const { formatDate } = require('../utils/jalali');
@@ -61,16 +63,21 @@ exports.uploadImage = (req, res) => {
   
   const upload = multer({
     limits: {fileSize: 4000000},
-    dest: "uploads/",
-    storage: storage,
+    // dest: "uploads/",
+    // storage: storage,
     fileFilter: fileFilter
   }).single("image");
 
-  upload(req, res, err => {
+  upload(req, res, async (err) => {
     if (err) {
       res.send(err);
     } else {
       if (req.file) {
+        // console.log(req.file);
+        const fileName = `${uuid()}_${req.file.originalname}`;
+        await sharp(req.file.buffer).jpeg({
+          quality: 60
+        }).toFile(`./public/uploads/${fileName}`).catch(err => console.log(err))
         res.status(200).send("آپلود عکس با موفقیت انجام شد");
       } else {
         res.send('جهت آپلود عکسی انتخاب کنید');
