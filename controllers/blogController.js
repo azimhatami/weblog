88b096e60cb6f5c9df2575da162cf3a6de935174
Azim Hatami <azimhatami.dev@gmail.com>
2025-06-12 00:45:46 +0330

    Create blog controller and homepage

---
+++ controllers/blogController.js
@@ -0,0 +1,19 @@
+const Blog = require('../models/Blog');
+const { formatDate } = require('../utils/jalali');
+
+
+exports.getIndex = async (req, res) => {
+  try {
+    const posts = await Blog.find({ status: 'public' }).sort({ createdAt: 'desc' });
+
+    res.render('index', {
+      pageTitle: 'وبلاگ',
+      path: '/',
+      posts,
+      formatDate
+    })
+  } catch (err) {
+    console.log(err);
+    res.render('errors/500');
+  }
+};
--- routes/blog.js
+++ routes/blog.js
@@ -1,16 +1,13 @@
 const { Router } = require('express');
 
+const blogController = require('../controllers/blogController');
+
 const router = new Router();
 
 
-// @desc Weblog Landing Page
+// @desc Weblog Index Page
 // @route GET /
-router.get('/', (req, res) => {
-  res.render('index', {
-    pageTitle: 'Weblog',
-    path: '/'
-  });
-});
+router.get('/', blogController.getIndex);
 
 
 module.exports = router;
