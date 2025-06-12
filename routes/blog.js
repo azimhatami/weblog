2fad59175afd005f49fa9a8194e1e1cfb61d1383
Azim Hatami <azimhatami.dev@gmail.com>
2025-06-12 10:56:31 +0330

    Display post content on page

--- routes/blog.js
+++ routes/blog.js
@@ -9,5 +9,9 @@ const router = new Router();
 // @route GET /
 router.get('/', blogController.getIndex);
 
+// @desc Weblog Post Page
+// @route GET /post/:id
+router.get('/post/:id', blogController.getSinglePost);
+
 
 module.exports = router;
