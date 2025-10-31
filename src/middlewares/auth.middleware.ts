cc06cb7b1edcb8bddc05f96fe07216ae4eb209e8
Azim Hatami <azimhatami.dev@gmail.com>
2025-11-01 00:05:15 +0330

    Add AuthRequest type to auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,6 +2,13 @@ import { Request, Response, NextFunction } from 'express';
 
 import { verifyToken } from '../utils/jwt';
 
+export interface AuthRequest extends Request {
+  user?: {
+    userId: number;
+    email: string;
+  }
+}
+
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
